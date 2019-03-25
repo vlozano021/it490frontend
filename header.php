2cd85b3e2635b52ce07206fe3324178dd621cf71
unknown <vl45@njit.edu>
2019-03-24 20:06:44 -0400

    add characterdashboard and modify header.php

--- header.php
+++ header.php
@@ -7,7 +7,6 @@ session_start();
 	<title>IT490 Midterm</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="js/scripts.js"></script>
 </head>
 <body>
 	<div class="navBar">
@@ -15,15 +14,8 @@ session_start();
 		<div class="dropdown">
 			<button class="dropbtn">Character &#9662;</button>
 			<div class="dropdownContent">
-				<a href="characterDashboard.php">Manage Characters</a>
+				<a href="characterdashboard.php">Manage Characters</a>
 				<a href="createCharacter.php">Create a Character</a>
-			</div>
-		</div>
-		<div class="dropdown">
-			<button class="dropbtn">Party &#9662;</button>
-			<div class="dropdownContent">
-				<a href="partyDashboard.php">Manage Parties</a>
-				<a href="party.php">Join a Party</a>
 			</div>
 		</div>
 		<a href="forums.php">Forums</a>
